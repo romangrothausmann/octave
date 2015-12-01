9c0b7c5b12b2885747764443e594b288cebdaa08
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2015-12-01 11:41:11 +0100

    looping over all possible pairs in jointSurf3D.m

--- scripts/jointSurf3D.m
+++ scripts/jointSurf3D.m
@@ -2,11 +2,18 @@
 ##script to execute imEuler3d from http://in.mathworks.com/matlabcentral/fileexchange/33690-geometric-measures-in-2d-3d-images
 
 
+####ToDo
+## check only once if label actually exists and store result for use in j-loop
+
 addpath ("~/octave/imMinkowski/");
 
+function pairId = CantorPairing(x, y)
+  pairId= (x+y)*(x+y+1)/2 + y;
+endfunction    
+
 arg_list = argv ();
-if nargin != 3
-  fprintf(stderr, "Usage: %s <input3D.mha> label1 label2 \n", program_name);
+if nargin != 1
+  fprintf(stderr, "Usage: %s <input3D.mha>\n", program_name);
   fprintf(stderr, "Decompressing MHAs/MHDs is very slow!\n");
   exit(1)
 else
@@ -15,7 +22,7 @@ else
   fprintf(stderr, "ElementSize: %f, %f, %f\n", i3dInfo.PixelDimensions);
   i3d= mha_read_volume(i3dInfo);#from ~/octave/functions/
   fprintf(stderr, "Reading done.\n", arg_list{1});
-  printf("Image size: %d %d %d\n", size(i3d))
+  fprintf(stderr, "Image size: %d %d %d\n", size(i3d))
   # if nargin == 2
   #   if (arg_list{2} == "-q")
   #     quiet= 1;
@@ -23,10 +30,34 @@ else
   # endif
 endif
 
-l1= str2num(arg_list{2})
-l2= str2num(arg_list{3})
+if ndims(i3d) ~= 3
+  error('Image has to be 3D!');
+end
 
-printf("jointSurface(13): %f\n", imJointSurface(i3d, l1, l2, 13, i3dInfo.PixelDimensions'))
+ll= min(min(min(i3d)));
+ul= max(max(max(i3d)));
+fprintf(stderr, "Image value range: %d to %d\n", ll, ul);
+
+printf("PairId\tL1\tL2\tjointSurf\n")
+
+for i= ll:ul
+  if isempty(find(i3d == i, 1)) #check if label value actually exists in i3d
+    fprintf(stderr, "%d not found, skipping.\n", i);
+    continue
+  endif
+
+  fprintf(stderr, "Evaluating adjacencies of label %d...\n", i);
+       
+  for j= i+1:ul
+    if isempty(find(i3d == j, 1)) #check if label value actually exists in i3d
+      fprintf(stderr, "%d not found, skipping.\n", i);
+      continue
+    endif
+
+    printf("%d\t%d\t%d\t%f\n", CantorPairing(i, j), i, j, imJointSurface(i3d, i, j, 13, i3dInfo.PixelDimensions'))
+    fprintf(stderr, "%d\t%d\t%d\t%f\n", CantorPairing(i, j), i, j, imJointSurface(i3d, i, j, 13, i3dInfo.PixelDimensions'))
+  end
+end
 
 
 
